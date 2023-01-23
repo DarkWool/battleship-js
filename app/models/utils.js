39e9163700fd450ec7b8c3738b37549c699783ad
DarkWool <cooper27@live.com.mx>
2023-01-22 23:26:50 -0600

    Add utils.test.js and getRandomCoords to utils.js

--- app/models/utils.js
+++ app/models/utils.js
@@ -5,8 +5,15 @@ function isNumber(num) {
     return (typeof num === "number" && !isNaN(num));
 }
 
+function getRandomCoords(max) {
+    const coordY = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+    const coordX = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+    return [coordY, coordX];
+};
+
 export {
     HORIZONTAL,
     VERTICAL,
     isNumber,
+    getRandomCoords
 };
