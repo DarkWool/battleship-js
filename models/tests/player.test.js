36f35047037c58b6b2e4ca85db06472cb538699f
DarkWool <cooper27@live.com.mx>
2023-02-04 18:19:48 -0600

    Fix and rewrite player.js tests

--- models/tests/player.test.js
+++ models/tests/player.test.js
@@ -3,15 +3,37 @@ import { player } from "../player.js";
 const playerA = player("Wool");
 const playerB = player("Fenix");
 
-describe("Name prop", () => {
-    test("Should have a 'name' property", () => {
-        expect(playerA).toHaveProperty("name");
-        expect(playerA.name).toBe("Wool");
-        expect(playerB.name).toBe("Fenix");
+describe("Players must have a", () => {
+    describe("'Name' prop", () => {
+        test("Must exist", () => {
+            expect(playerA).toHaveProperty("name");
+            expect(playerA.name).toBe("Wool");
+            expect(playerB.name).toBe("Fenix");
+        });
     });
+    
+    describe("'Board' prop", () => {
+        test("Must exist", () => {
+            expect(playerA).toHaveProperty("board");
+            expect(playerB).toHaveProperty("board");
+        });
+    });
+});
 
-    test(`Fails to create a player when 'name' is not a string 
-    or it's an empty string`, () => {
+describe("attack()", () => {
+    test("Should call the 'receiveAttack' method of the enemy board", () => {
+        const receiveAttack = jest.spyOn(playerB.board, "receiveAttack");
+        playerA.attack(playerB.board, [2, 5]);
+        playerA.attack(playerB.board, [0, 8]);
+        playerA.attack(playerB.board, [9, 9]);
+        
+        expect(receiveAttack).toHaveBeenCalled();
+        expect(receiveAttack).toHaveBeenCalledTimes(3);
+    });
+});
+
+describe("Doesn't creates a new player", () => {
+    test("When 'name' argument is not a string or it's an empty string", () => {
         expect(player(123)).toBeFalsy();
         expect(player(0)).toBeFalsy();
         expect(player(["DarkWool"])).toBeFalsy();
@@ -20,27 +42,5 @@ describe("Name prop", () => {
         expect(player(null)).toBeFalsy();
         expect(player(undefined)).toBeFalsy();
         expect(player(true)).toBeFalsy();
-    });
-});
-
-describe("Board prop", () => {
-    test("Should have a 'board' property", () => {
-        expect(playerA).toHaveProperty("board");
-        expect(playerB).toHaveProperty("board");
-    });
-});
-
-describe("attack()", () => {
-    test("It shouldn't shoot to the same coords twice", () => {
-        playerA.attack(playerB.board, [2, 5]);
-        playerA.attack(playerB.board, [0, 8]);
-        playerA.attack(playerB.board, [9, 9]);
-        
-        const receiveAttack = jest.spyOn(playerB.board, "receiveAttack");
-        playerA.attack(playerB.board, [2, 5]);
-        playerA.attack(playerB.board, [0, 8]);
-        playerA.attack(playerB.board, [9, 9]);
-
-        expect(receiveAttack).not.toHaveBeenCalled();
     });
 });
